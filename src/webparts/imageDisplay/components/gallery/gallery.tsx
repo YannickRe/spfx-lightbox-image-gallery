f8774b3f309b28a5007ac88665af71e7331169c4
CreativeAcer <marco.moris@outlook.com>
2020-08-05 23:25:26 +0200

    base working unit nav - folder - image grid - image lightbox

--- src/webparts/imageDisplay/components/gallery/gallery.tsx
+++ src/webparts/imageDisplay/components/gallery/gallery.tsx
@@ -14,20 +14,19 @@ export default class Gallery extends React.Component<IGalleryProps, {}> {
   
   constructor(props: any){
     super(props);
-
-    this.gridColumnImageBuild = this.gridColumnImageBuild.bind(this);
-    this.gridContainerBuild = this.gridContainerBuild.bind(this);
   }
 
   public render(): React.ReactElement<IGalleryProps> {
     return (
-      <div className={styles["grid-container"]} style={this.gridSize}>
-        <div className={styles.grid}>
-            {
-              this.gridContainerBuild()
-            }
-        </div>
-      </div>      
+      <div style={this.gridSize}>
+        <div className={styles["image-list"]}>
+          {
+            this.props.photos.map((_photo) => {
+                return <Image photo={_photo}></Image>
+            })
+          }
+        </div> 
+      </div>  
     );
   }
 
@@ -41,34 +40,5 @@ export default class Gallery extends React.Component<IGalleryProps, {}> {
     
   }
 
-  gridContainerBuild(){
-    let chunkedArray = this.chunkArray(this.props.photos, this.gridColumnSize);  
-    return chunkedArray.map((photoArray) => {
-      return <div className={styles.column}>
-        {
-          this.gridColumnImageBuild(photoArray)
-        }
-      </div>
-    })
-  }
-  gridColumnImageBuild(photoItems: Photo[]): any {
-    return photoItems.map((_photo) => {
-      return <Image photo={_photo}></Image>
-    })
-  }
-
-  // divide images into equal parts
-  chunkArray(imgArray: Photo[], chunk_size: number){
-    let index = 0;
-    let arrayLength = imgArray.length;
-    let tempArray = [];
-    
-    for (index = 0; index < arrayLength; index += chunk_size) {
-        let myChunk = imgArray.slice(index, index+chunk_size);
-        // Do something if you want with the group
-        tempArray.push(myChunk);
-    }
-
-    return tempArray;
-  }
+  
 }
--- src/webparts/imageDisplay/components/gallery/image/image.tsx
+++ src/webparts/imageDisplay/components/gallery/image/image.tsx
@@ -3,22 +3,36 @@ import { IImageProps } from './imageProps';
 // import styles from './image.module.scss';
 import styles from '../gallery.module.scss';
 
+export interface IImageState {
+  spans: any
+}
 
-export default class Gallery extends React.Component<IImageProps, {}> {
+export default class Gallery extends React.Component<IImageProps, IImageState> {
 
-  private backURL:React.CSSProperties = { backgroundImage: `url('${this.props.photo.photo}')` };
+  // private backURL:React.CSSProperties = { backgroundImage: `url('${this.props.photo.photo}')` };
+  private imageRef: any;
   
   constructor(props: any){
     super(props);  
-  }  
+    this.state = {spans:0};
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10 );
+    this.setState({ spans: spans});
+  }
 
   public render(): React.ReactElement<IImageProps> {
     return (
-      <figure className={`${styles.picture} ${styles.loaded}`}>
-        <div className={styles.bg} style={this.backURL}>
-          <img src={this.props.photo.photo} />
-        </div>
-      </figure> 
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={this.props.photo.photo} />
+      </div>
     );
   }
 }
