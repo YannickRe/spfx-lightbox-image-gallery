3966572c0ab5d76b1807c1516a16664482b2458d
CreativeAcer <marco.moris@outlook.com>
2020-08-20 14:23:11 +0200

    removed img reference and updated gallery rendering - css based no longer js based

--- src/webparts/imageDisplay/components/gallery/image/image.tsx
+++ src/webparts/imageDisplay/components/gallery/image/image.tsx
@@ -4,32 +4,16 @@ import { IImageProps } from './imageProps';
 import styles from '../gallery.module.scss';
 
 export interface IImageState {
-  spans: any;
   classN: any;
 }
 
 export default class Gallery extends React.Component<IImageProps, IImageState> {
-
-  // private backURL:React.CSSProperties = { backgroundImage: `url('${this.props.photo.photo}')` };
-  private imageRef: any;
   
   constructor(props: any){
     super(props);  
     this.state = {
-      spans:0,
       classN: ""
     };
-    this.imageRef = React.createRef();
-  }
-
-  componentDidMount() {
-    this.imageRef.current.addEventListener("load", this.setSpans);
-  }
-
-  setSpans = () => {
-    const height = this.imageRef.current.clientHeight;
-    const spans = Math.ceil(height / 10 );
-    this.setState({ spans: spans});
   }
 
   setHover = (bool) => {
@@ -38,8 +22,8 @@ export default class Gallery extends React.Component<IImageProps, IImageState> {
 
   public render(): React.ReactElement<IImageProps> {
     return (
-      <div onMouseEnter={() => this.setHover(true)} onMouseLeave={() => this.setHover(false)} onClick={(e) => this.props.photoClicked(this.props.index)} className={this.state.classN} style={{ gridRowEnd: `span ${this.state.spans}` }}>
-        <img ref={this.imageRef} src={this.props.photo.photo} />
+      <div onMouseEnter={() => this.setHover(true)} onMouseLeave={() => this.setHover(false)} onClick={(e) => this.props.photoClicked(this.props.index)} className={this.state.classN} >
+        <img src={this.props.photo.photo} />
       </div>
     );
   }
