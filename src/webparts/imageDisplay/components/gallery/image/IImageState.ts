6b1247fc9643ec054a6225d028109978b094f66f
CreativeAcer <marco.moris@outlook.com>
2020-09-14 21:29:13 +0200

    added image on hover effect

--- src/webparts/imageDisplay/components/gallery/image/IImageState.ts
+++ src/webparts/imageDisplay/components/gallery/image/IImageState.ts
@@ -1,3 +1,4 @@
 export interface IImageState {
     classN: any;
+    classSpan: any;
   }
--- src/webparts/imageDisplay/components/gallery/image/image.tsx
+++ src/webparts/imageDisplay/components/gallery/image/image.tsx
@@ -10,18 +10,19 @@ export default class Gallery extends React.Component<IImageProps, IImageState> {
   constructor(props: any){
     super(props);  
     this.state = {
-      classN: ""
+      classN: "",
+      classSpan: styles.hiddenspan
     };
   }
 
   setHover = (bool) => {
-    this.setState({ classN: bool ? styles.zoom : ""});
+    this.setState({ classN: bool ? styles.zoom : "", classSpan:bool ? styles.visiblespan : styles.hiddenspan});
   }
 
   public render(): React.ReactElement<IImageProps> {
     return (
-      <div onMouseEnter={() => this.setHover(true)} onMouseLeave={() => this.setHover(false)} onClick={(e) => this.props.photoClicked(this.props.index)} className={this.state.classN} >
-        <img src={this.props.photo.photo} />
+      <div onMouseEnter={() => this.setHover(true)} onMouseLeave={() => this.setHover(false)} onClick={(e) => this.props.photoClicked(this.props.index)} className={styles.zoom} >
+        <img src={this.props.photo.photo} /><span className={this.state.classSpan}>{this.props.photo.subcaption}</span>
       </div>
     );
   }
